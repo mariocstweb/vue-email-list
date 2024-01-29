6e3f6ca3d365ba2e74d58e3bed2ce8f5ff0022d2
mariocstweb <mariocstmra@gmail.com>
2024-01-29 15:32:58 +0100

    imported endpoint

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 console.log("ok", Vue);
 
 const { createApp } = Vue;
+// Importo l endpoint
+const endpoint = "https://flynn.boolean.careers/exercises/api/random/mail";
 const app = createApp({
   data: () => ({}),
 });
