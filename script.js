9530ab0e6cdd480fd10a9bd4c1828893e0cdd2c0
mariocstweb <mariocstmra@gmail.com>
2024-01-29 15:52:20 +0100

    added axios and v-for to generate a email lists

--- script.js
+++ script.js
@@ -5,8 +5,17 @@ const { createApp } = Vue;
 const endpoint = "https://flynn.boolean.careers/exercises/api/random/mail";
 const app = createApp({
   data: () => ({
-    email: [],
+    emails: [],
   }),
+  created() {
+    // Utilizzo un ciclo for per ripetere l operazione tot volte
+    for (let i = 0; i < 10; i++) {
+      axios.get(endpoint).then((res) => {
+        // Utilizzo il metodo push per "riempire" l'array vuoto emails con cioò che ottengo dalla chiamata ajax
+        this.emails.push(res.data.response);
+      });
+    }
+  },
 });
 
 app.mount("#root");
